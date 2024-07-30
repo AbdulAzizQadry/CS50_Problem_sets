a950d4855ac1a502ff645932edaeab422ff7d5fb
AbdulAzizQadry <sezaroo2004@hotmail.com>
2024-07-31 00:30:39 +0300

    Add files via upload

---
+++ Week_2/caesar.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include "cs50.h"
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+bool only_digits (string key);
+string rotate (string plain, int key);
+
+int main (int argc, string argv [])
+{
+    if (argc != 2 || only_digits(argv [1]))
+    {
+        printf ("Usage: ./caesar key\n");
+        return 1;
+    }
+
+    int key = atoi (argv [1]);
+    key %= 26;
+
+    string plain = get_string ("plaintext:  ");
+    string cipher = rotate (plain, key);
+    printf ("ciphertext: %s", cipher);
+    return 0;
+}
+
+bool only_digits (string key)
+{
+    for (int i = 0, n = strlen (key); i < n; i++)
+    {
+        if (isdigit (key[i]) == 0)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+string rotate (string plain, int key)
+{
+    string cipher = plain;
+    for (int i = 0, n = strlen (plain); i < n; i++)
+    {
+        if (isalpha (plain [i]))
+        {
+            char offset = isupper (plain [i]) ? 'A' : 'a';
+            cipher [i] = ((plain [i] - offset + key) % 26) + offset;
+        }
+    }
+    return cipher;
+}
---
+++ Week_2/readability.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "cs50.h"
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+int coleman_liao (string text);
+
+int main (void)
+{
+    string text = get_string ("Text: ");
+    
+    int grade = coleman_liao (text);
+
+    if (grade <= 1)
+    {
+        printf ("Before Grade 1\n");
+    }
+    else if (grade < 16)
+    {
+        printf ("Grade %i", grade);
+    }
+    else
+    {
+        printf ("Grade 16+\n");
+    }
+}
+
+int coleman_liao (string text)
+{
+    int letters = 0;
+    int words = 1;
+    int sentences = 0;
+
+    for (int i = 0, n = strlen (text); i < n; i++)
+    {
+        if (isalpha (text [i]))
+        {
+            letters++;
+        }
+        else if (isblank (text [i]))
+        {
+            words++;
+        }
+        else if (text [i] == '.' || text [i] == '?' || text [i] == '!')
+        {
+            sentences++;
+        }
+    }
+
+    int index = round ((0.0588 * letters / words * 100) - (0.296 * sentences / words * 100) - 15.8);
+    return index;
+}
